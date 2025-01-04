b44de78237ab61c676281dfd4a317bc1a4eede09
sven-hash <>
2025-01-04 15:03:21 +0100

    add: new

--- app/src/pages/deposit.tsx
+++ app/src/pages/deposit.tsx
@@ -7,7 +7,7 @@ export default function Deposit() {
     <>
       <Head>
         <title>Deposit</title>
-        <meta name="description" content="Deposit page for Token Faucet" />
+        <meta name="description" content="Deposit page for new token." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <>
       <Head>
         <title>Token convert</title>
-        <meta name="description" content="Convert tokens to new one." />
+        <meta name="description" content="Convert token to new one." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
